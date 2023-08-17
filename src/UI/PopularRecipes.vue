7f01f885e977061ee98d1d9a48066215055db24f
Екатерина Дорофеева <multiveb@mail.ru>
2023-08-17 11:24:25 +0300

    Создание популярного на главной странице (продолжается 5) + создание массива рецептов

--- src/UI/PopularRecipes.vue
+++ src/UI/PopularRecipes.vue
@@ -11,10 +11,10 @@
 
     <section class="grid grid-cols-1 gap-7">
       <!-- Первый рецепт -->
-      <div class="shadow-card-main rounded-b-xl">
+      <div class="shadow-card-main rounded-b-xl" v-for="recipe in recipeDetails.recipes" :key="recipe.id">
         <div class="relative">
           <section class="flex items-center absolute left-5 bottom-2.5">
-            <div class="bg-category rounded-br-ten text-xs font-semibold px-category-hashtag-x py-category-hashtag-y">СУПЫ</div>
+            <div class="bg-category rounded-br-ten text-xs font-semibold px-category-hashtag-x py-category-hashtag-y">{{recipe.category}}</div>
             <div class="[&:not(:first-child)]:ml-2 bg-category-hashtag rounded-br-ten text-xs font-semibold text-white px-category-hashtag-x py-category-hashtag-y">#Хит</div>
           </section>
           <img src="/public/img/popular/soup-puree.png" class="rounded-t-xl">
@@ -22,13 +22,13 @@
 
         <div class="py-5">
           <div class="px-5 mb-3.5">
-            <div class="text-xl font-semibold leading-card-title mb-2">Суп-пюре из тыквы со сливками</div>
-            <p class="text-justify leading-card-text mb-4">Тыква, картошка, морковь, репчатый лук, чеснок, сливочное масло, сливки 10% жирности, соль, специи</p>
+            <div class="text-xl font-semibold leading-card-title mb-2">{{recipe.title}}</div>
+            <p class="text-justify leading-card-text mb-4">{{recipe.description}}</p>
 
             <div class="flex items-center" @click="recipeDetails.enableActivity">
               <Icon icon="bx:food-menu" color="#f4b990" width="19" height="19" />
               <p class="text-more ml-2 font-medium">Подробнее</p>
-              <Icon icon="ep:arrow-up-bold" class="ml-3 flip_arrow_hidden" :class="[{flip_arrow_active: !recipeDetails.listActivity}]" color="rgba(33,33,33,.4)" width="18" height="18" />
+              <Icon icon="ep:arrow-up-bold" class="ml-3 ease-in duration-200" :class="[{flip_arrow_active: !recipeDetails.listActivity}]" color="rgba(33,33,33,.4)" width="18" height="18" />
             </div>
           </div>
 
@@ -39,15 +39,15 @@
             <div class="flex items-center justify-between px-5 mb-4">
               <div class="flex items-center">
                 <Icon icon="bi:pie-chart-fill" color="#f4b990" width="19" height="19" />
-                <p class="text-more ml-2 font-medium">74,26 кКал</p>
+                <p class="text-more ml-2 font-medium">{{recipe.calories}}</p>
               </div>
               <div class="flex items-center">
                 <Icon icon="emojione-monotone:pot-of-food" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">Легко</p>
+                <p class="text-more ml-2 font-medium">{{recipe.complexity}}</p>
               </div>
               <div class="flex items-center">
                 <Icon icon="wi:time-3" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">40 мин.</p>
+                <p class="text-more ml-2 font-medium">{{recipe.time}}.</p>
               </div>
             </div>
 
@@ -56,15 +56,15 @@
             <div class="flex items-center justify-between px-5 mb-4">
               <div class="flex items-center">
                 <Icon icon="file-icons:dna" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">1,73 г.</p>
+                <p class="text-more ml-2 font-medium">{{recipe.proteins}}.</p>
               </div>
               <div class="flex items-center">
                 <Icon icon="ph:drop-bold" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">3,97 г.</p>
+                <p class="text-more ml-2 font-medium">{{recipe.fats}}.</p>
               </div>
               <div class="flex items-center">
                 <Icon icon="lucide:wheat" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">9,05 г.</p>
+                <p class="text-more ml-2 font-medium">{{recipe.carbohydrates}}.</p>
               </div>
             </div>
 
@@ -73,7 +73,10 @@
 
           <div class="flex items-center justify-between px-5">
             <div class="flex items-center">
-              <Icon icon="ph:heart-bold" color="#dd727b" width="30" height="30" />
+              <div @click="recipeDetails.clickHeart">
+                <Icon icon="ph:heart-bold" v-if="recipeDetails.heartActivity === false" color="#dd727b" width="30" height="30" />
+                <Icon icon="ph:heart-fill" v-else color="#dd727b" width="30" height="30" />
+              </div>
               <div class="flex items-center">
                 <Icon icon="ph:star-fill" class="ml-6" color="#f7d000" width="30" height="30" />
                 <p class="text-grade-star font-medium ml-2">5,0</p>
@@ -86,80 +89,83 @@
       </div>
 
       <!-- Второй рецепт -->
-      <div class="shadow-card-main rounded-b-xl">
-        <div class="relative">
-          <section class="flex items-center absolute left-5 bottom-2.5">
-            <div class="bg-category rounded-br-ten text-xs font-semibold px-category-hashtag-x py-category-hashtag-y">САЛАТЫ</div>
-            <div class="[&:not(:first-child)]:ml-2 bg-category-hashtag rounded-br-ten text-xs font-semibold text-white px-category-hashtag-x py-category-hashtag-y">#Хит</div>
-            <div class="[&:not(:first-child)]:ml-2 bg-category-hashtag rounded-br-ten text-xs font-semibold text-white px-category-hashtag-x py-category-hashtag-y">#Готовить легко</div>
-          </section>
-          <img src="/public/img/popular/salad-shrimp.png" class="rounded-t-xl">
-        </div>
+<!--      <div class="shadow-card-main rounded-b-xl">-->
+<!--        <div class="relative">-->
+<!--          <section class="flex items-center absolute left-5 bottom-2.5">-->
+<!--            <div class="bg-category rounded-br-ten text-xs font-semibold px-category-hashtag-x py-category-hashtag-y">САЛАТЫ</div>-->
+<!--            <div class="[&:not(:first-child)]:ml-2 bg-category-hashtag rounded-br-ten text-xs font-semibold text-white px-category-hashtag-x py-category-hashtag-y">#Хит</div>-->
+<!--            <div class="[&:not(:first-child)]:ml-2 bg-category-hashtag rounded-br-ten text-xs font-semibold text-white px-category-hashtag-x py-category-hashtag-y">#Готовить легко</div>-->
+<!--          </section>-->
+<!--          <img src="/public/img/popular/salad-shrimp.png" class="rounded-t-xl">-->
+<!--        </div>-->
 
-        <div class="py-5">
-          <div class="px-5 mb-3.5">
-            <div class="text-xl font-semibold leading-card-title mb-2">Салат из креветок с огурцом и яйцом</div>
-            <p class="text-justify leading-card-text mb-4">Креветки, куриное яйцо, огурец, твердый сыр, майонез, соль, укроп</p>
+<!--        <div class="py-5">-->
+<!--          <div class="px-5 mb-3.5">-->
+<!--            <div class="text-xl font-semibold leading-card-title mb-2">Салат из креветок с огурцом и яйцом</div>-->
+<!--            <p class="text-justify leading-card-text mb-4">Креветки, куриное яйцо, огурец, твердый сыр, майонез, соль, укроп</p>-->
 
-            <div class="flex items-center" @click="recipeDetails.enableActivity">
-              <Icon icon="bx:food-menu" color="#f4b990" width="19" height="19" />
-              <p class="text-more ml-2 font-medium">Подробнее</p>
-              <Icon icon="ep:arrow-up-bold" class="ml-3 flip_arrow_hidden" :class="[{flip_arrow_active: !recipeDetails.listActivity}]" color="rgba(33,33,33,.4)" width="18" height="18" />
-            </div>
-          </div>
+<!--            <div class="flex items-center" @click="recipeDetails.enableActivity">-->
+<!--              <Icon icon="bx:food-menu" color="#f4b990" width="19" height="19" />-->
+<!--              <p class="text-more ml-2 font-medium">Подробнее</p>-->
+<!--              <Icon icon="ep:arrow-up-bold" class="ml-3 ease-in duration-200" :class="[{flip_arrow_active: !recipeDetails.listActivity}]" color="rgba(33,33,33,.4)" width="18" height="18" />-->
+<!--            </div>-->
+<!--          </div>-->
 
-          <hr class="h-px bg-hr w-full mb-4">
+<!--          <hr class="h-px bg-hr w-full mb-4">-->
 
-          <!-- Выпадающий список (кКал, сложность, время, БЖУ) -->
-          <div class="mb-6" v-if="recipeDetails.listActivity === false">
-            <div class="flex items-center justify-between px-5 mb-4">
-              <div class="flex items-center">
-                <Icon icon="bi:pie-chart-fill" color="#f4b990" width="19" height="19" />
-                <p class="text-more ml-2 font-medium">201,92 кКал</p>
-              </div>
-              <div class="flex items-center">
-                <Icon icon="emojione-monotone:pot-of-food" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">Легко</p>
-              </div>
-              <div class="flex items-center">
-                <Icon icon="wi:time-3" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">15 мин.</p>
-              </div>
-            </div>
+<!--          &lt;!&ndash; Выпадающий список (кКал, сложность, время, БЖУ) &ndash;&gt;-->
+<!--          <div class="mb-6" v-if="recipeDetails.listActivity === false">-->
+<!--            <div class="flex items-center justify-between px-5 mb-4">-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="bi:pie-chart-fill" color="#f4b990" width="19" height="19" />-->
+<!--                <p class="text-more ml-2 font-medium">201,92 кКал</p>-->
+<!--              </div>-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="emojione-monotone:pot-of-food" color="#f4b990" width="22" height="22" />-->
+<!--                <p class="text-more ml-2 font-medium">Легко</p>-->
+<!--              </div>-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="wi:time-3" color="#f4b990" width="22" height="22" />-->
+<!--                <p class="text-more ml-2 font-medium">15 мин.</p>-->
+<!--              </div>-->
+<!--            </div>-->
 
-            <hr class="h-px bg-hr w-full mb-4">
+<!--            <hr class="h-px bg-hr w-full mb-4">-->
 
-            <div class="flex items-center justify-between px-5 mb-4">
-              <div class="flex items-center">
-                <Icon icon="file-icons:dna" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">12,34 г.</p>
-              </div>
-              <div class="flex items-center">
-                <Icon icon="ph:drop-bold" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">15,99 г.</p>
-              </div>
-              <div class="flex items-center">
-                <Icon icon="lucide:wheat" color="#f4b990" width="22" height="22" />
-                <p class="text-more ml-2 font-medium">2,41 г.</p>
-              </div>
-            </div>
+<!--            <div class="flex items-center justify-between px-5 mb-4">-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="file-icons:dna" color="#f4b990" width="22" height="22" />-->
+<!--                <p class="text-more ml-2 font-medium">12,34 г.</p>-->
+<!--              </div>-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="ph:drop-bold" color="#f4b990" width="22" height="22" />-->
+<!--                <p class="text-more ml-2 font-medium">15,99 г.</p>-->
+<!--              </div>-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="lucide:wheat" color="#f4b990" width="22" height="22" />-->
+<!--                <p class="text-more ml-2 font-medium">2,41 г.</p>-->
+<!--              </div>-->
+<!--            </div>-->
 
-            <hr class="h-px bg-hr w-full">
-          </div>
+<!--            <hr class="h-px bg-hr w-full">-->
+<!--          </div>-->
 
-          <div class="flex items-center justify-between px-5">
-            <div class="flex items-center">
-              <Icon icon="ph:heart-bold" color="#dd727b" width="30" height="30" />
-              <div class="flex items-center">
-                <Icon icon="ph:star-fill" class="ml-6" color="#f7d000" width="30" height="30" />
-                <p class="text-grade-star font-medium ml-2">4,8</p>
-              </div>
-            </div>
+<!--          <div class="flex items-center justify-between px-5">-->
+<!--            <div class="flex items-center">-->
+<!--              <div @click="recipeDetails.clickHeart">-->
+<!--                <Icon icon="ph:heart-bold" v-if="recipeDetails.heartActivity === false" color="#dd727b" width="30" height="30" />-->
+<!--                <Icon icon="ph:heart-fill" v-else color="#dd727b" width="30" height="30" />-->
+<!--              </div>-->
+<!--              <div class="flex items-center">-->
+<!--                <Icon icon="ph:star-fill" class="ml-6" color="#f7d000" width="30" height="30" />-->
+<!--                <p class="text-grade-star font-medium ml-2">4,8</p>-->
+<!--              </div>-->
+<!--            </div>-->
 
-            <button class="px-4 py-1.5 bg-orange text-white font-semibold rounded-br-ten">К рецепту</button>
-          </div>
-        </div>
-      </div>
+<!--            <button class="px-4 py-1.5 bg-orange text-white font-semibold rounded-br-ten">К рецепту</button>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
     </section>
   </article>
 </template>
@@ -177,16 +183,22 @@
     name: 'PopularRecipes',
     components: {
       Icon
+    },
+    data() {
+      return {
+        heartActivity: false
+      }
+    },
+    methods: {
+      clickHeart() {
+
+      }
     }
   }
 </script>
 
 <style scoped lang="scss">
-  .flip_arrow_hidden {
-    @apply ease-in duration-300
-  }
-
   .flip_arrow_active {
-    @apply rotate-180 ease-in duration-300
+    @apply rotate-180 ease-in duration-200
   }
 </style>
--- src/store/RecipeDetails.js
+++ src/store/RecipeDetails.js
@@ -3,9 +3,26 @@ import {ref} from "vue";
 
 // Подробно о рецепте (выпадающий список)
 export const useRecipeDetails = defineStore('recipeDetails', () => {
+    const recipes = ref([
+        {
+            id: 1,
+            announcement: '/soup-puree',
+            category: 'СУПЫ',
+            title: 'Суп-пюре из тыквы со сливками',
+            description: 'Тыква, картошка, морковь, репчатый лук, чеснок, сливочное масло, сливки 10% жирности, соль, специи',
+            calories: '74,26',
+            complexity: 'Легко',
+            time: '40 мин',
+            proteins: '1,73 г',
+            fats: '3,97 г',
+            carbohydrates: '9,05 г',
+            toFavorites: false,
+            grade: '5,0'
+        },
+    ])
+
     // Активность выпадающего списка
     const listActivity = ref(true);
-
     const enableActivity = () => {
         if (listActivity.value === true) {
             listActivity.value = false
@@ -14,7 +31,18 @@ export const useRecipeDetails = defineStore('recipeDetails', () => {
         }
     }
 
+    // Кликнуть по сердечку в карточке товара (В избранное)
+    // const heartActivity = ref(false);
+    // const clickHeart = () => {
+    //     if (heartActivity.value === false) {
+    //         heartActivity.value = true
+    //     } else  {
+    //         heartActivity.value = false
+    //     }
+    // }
+
     return {
-        listActivity, enableActivity
+        recipes, listActivity, enableActivity
+        //heartActivity, clickHeart
     }
 });
