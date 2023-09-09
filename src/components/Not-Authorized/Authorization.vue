ef587d43395a2a9021b53b185b58d221e1245172
Екатерина Дорофеева <multiveb@mail.ru>
2023-09-09 21:18:11 +0300

    Разработка страницы авторизации (оповещения при незаполненных полях)

--- src/components/Not-Authorized/Authorization.vue
+++ src/components/Not-Authorized/Authorization.vue
@@ -12,19 +12,27 @@
       <h1 class="text-2xl font-semibold mb-6">Авторизация</h1>
 
       <form>
-        <label class="field__item mb-5">
-          <input type="text" class="entry__field" v-model="input_login" required/>
-          <span class="label__title">Логин</span>
-          <img src="/public/img/clear.svg" class="cross__close" @click="input_login=''" alt="Крестик">
-        </label>
+        <div class="mb-5">
+          <label class="field__item mb-2">
+            <input type="text" class="entry__field" v-model="input_login" required oninvalid="this.setCustomValidity(' ')" oninput="setCustomValidity('')"/>
+            <span class="label__title">Логин</span>
+            <img src="/public/img/clear.svg" class="cross__close" @click="input_login=''" alt="Крестик">
+          </label>
+
+          <p v-if="required_login === true && input_login === ''" class="text-sm text-required-field">Заполните поле "Логин"</p>
+        </div>
 
         <p class="flex justify-end font-medium text-[14px] text-orange mb-1">Забыли пароль?</p>
 
-        <label class="field__item mb-5">
-          <input type="password" class="entry__field" v-model="input_password" required/>
-          <span class="label__title">Пароль</span>
-          <img src="/public/img/clear.svg" class="cross__close" @click="input_password=''" alt="Крестик">
-        </label>
+        <div class="mb-5">
+          <label class="field__item mb-2">
+            <input type="password" class="entry__field" v-model="input_password" required oninvalid="this.setCustomValidity(' ')" oninput="setCustomValidity('')"/>
+            <span class="label__title">Пароль</span>
+            <img src="/public/img/clear.svg" class="cross__close" @click="input_password=''" alt="Крестик">
+          </label>
+
+          <p v-if="required_password === true && input_password === ''" class="text-sm text-required-field">Заполните поле "Пароль"</p>
+        </div>
 
         <div class="flex items-center gap-2">
           <p class="text-[rgba(0,0,0,.8)] font-medium">Нет аккаунта? Давайте
@@ -33,7 +41,7 @@
           <Icon icon="mingcute:arrow-up-fill" class="rotate-90" color="#f68b31" width="18" height="18" />
         </div>
 
-        <button class="btn absolute bottom-0" :class="[{btn__active: input_login !== '' && input_password !== ''}]">
+        <button class="btn absolute bottom-0" @click="requiredField" :class="[{btn__active: input_login !== '' && input_password !== ''}]">
           Войти
         </button>
       </form>
@@ -54,7 +62,19 @@
     data() {
       return {
         input_login: '',
-        input_password: ''
+        input_password: '',
+        required_login: false,
+        required_password: false
+      }
+    },
+    methods: {
+      requiredField() {
+        if (this.required_login === false) {
+          this.required_login = true
+        }
+        if (this.required_password === false) {
+          this.required_password = true
+        }
       }
     }
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ export default {
         'more': 'rgba(0,0,0,.7)',
         'hr': 'rgba(237,237,237,.5)',
         'empty-field': '#F8F8F8',
-        'inactive-button': '#F8F8F8'
+        'inactive-button': '#F8F8F8',
+        'required-field': '#D93838'
       },
     },
   },
